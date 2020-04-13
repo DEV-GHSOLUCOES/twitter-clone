04ca444cab7e93834b163c07de0be505c59500a1
DevGuima <glecioheybel@hotmail.com>
2020-04-13 16:47:54 -0300

    Teste de ambiente

---
+++ twitterclone/src/main/java/br/com/twitterclone/TwittercloneApplication.java
@@ -0,0 +1,13 @@
+package br.com.twitterclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwittercloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwittercloneApplication.class, args);
+	}
+
+}
---
+++ twitterclone/src/test/java/br/com/twitterclone/TwittercloneApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.twitterclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwittercloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
